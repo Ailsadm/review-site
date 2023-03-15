ff80e338eb580a790d87c7b51950c3aa6f38ab0c
meri <mariana_asavoaiei@outlook.com>
2023-03-15 20:06:36 +0000

    second api for books

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={response.cover} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and keep on practicing
         </a>
       </header>
     </div>
@@ -23,3 +26,4 @@ function App() {
 }
 
 export default App;
+
