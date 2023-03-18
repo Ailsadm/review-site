ec3c272ec06b374d35501468611b17cec4509c90
Ailsadm <ailsamcgowan@hotmail.co.uk>
2023-03-18 18:05:03 +0000

    code updated so onClick takes you to new page component

--- src/components/Books/books.js
+++ src/components/Books/books.js
@@ -1,23 +1,53 @@
-import React from "react";
-// copied the style.css from booksGallery as this is where we re displaying the results
+// import React from "react";
+// // copied the style.css from booksGallery as this is where we re displaying the results
+// import "./style.css";
+// import Reviews from "../Reviews/Reviews";
+
+// function Books({ bookData }) {
+//   console.log(bookData);
+//   async function ReviewsFetch() {
+//     // console.log();
+//     //SaoMEmUz0tfY5UL7hX6ivJM7EVAWm5zz
+//     //old 22071f1160msh3cb38f59ce444bbp11980ajsn519917436eb7
+//     // let bookTitle = bookData.name.split(" ").join("+")
+//     let bookTitle = bookData.name.replaceAll(' ', '+');
+    
+//     fetch(
+//       `https://api.nytimes.com/svc/books/v3/reviews.json?title=${bookTitle}&api-key=SaoMEmUz0tfY5UL7hX6ivJM7EVAWm5zz`
+//     )
+//       .then((response) => response.json())
+//       .then((response) => console.log(response));
+//   }
+//   return (
+//     <div className="row container">
+//       <div className="card">
+//         <div>
+//           <img className="bookCover" src={bookData.cover} alt={bookData.name} />
+//           <p>Name: {bookData.name}</p>
+//           <p>Author: {bookData.authors}</p>
+//           <p>Year: {bookData.year}</p>
+//           {/* /*<p>Goodreads link: {bookData.url}</p>*/}
+//           <button onClick={ReviewsFetch}>Reviews</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Books;
+
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import Reviews from "../Reviews/Reviews";
 import "./style.css";
-import Reviews from "../Reviews/reviews"
 
 function Books({ bookData }) {
-  console.log(bookData);
-  async function ReviewsFetch() {
-    // console.log();
-    //SaoMEmUz0tfY5UL7hX6ivJM7EVAWm5zz
-    //old 22071f1160msh3cb38f59ce444bbp11980ajsn519917436eb7
-    // let bookTitle = bookData.name.split(" ").join("+")
-    let bookTitle = bookData.name.replaceAll(' ', '+');
-    
-    fetch(
-      `https://api.nytimes.com/svc/books/v3/reviews.json?title=${bookTitle}&api-key=SaoMEmUz0tfY5UL7hX6ivJM7EVAWm5zz`
-    )
-      .then((response) => response.json())
-      .then((response) => console.log(response));
+  const navigate = useNavigate();
+
+  const handleClick = e => {
+    navigate('/reviews');
   }
+
   return (
     <div className="row container">
       <div className="card">
@@ -26,8 +56,7 @@ function Books({ bookData }) {
           <p>Name: {bookData.name}</p>
           <p>Author: {bookData.authors}</p>
           <p>Year: {bookData.year}</p>
-          {/* /*<p>Goodreads link: {bookData.url}</p>*/}
-          <button onClick={ReviewsFetch}>Reviews</button>
+          <button onClick={handleClick}>Reviews</button>
         </div>
       </div>
     </div>
--- src/components/Reviews/reviews.js
+++ src/components/Reviews/reviews.js
@@ -1,18 +1,13 @@
 import React from "react";
 import "./style.css";
-// import Books from "../Books/books";
 
-
-// move the fetch for revies in reviews
 
 function Reviews(){
-      
-      
     return (
         <div>
             <p>Test</p>
         </div>
-    )
+    );
 }
 
-export default Reviews()
+export default Reviews;
