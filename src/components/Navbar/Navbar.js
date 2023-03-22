fa3397a3e7fd5b5d55a236b3b9c47708e5a507ca
Ailsadm <ailsamcgowan@hotmail.co.uk>
2023-03-22 18:49:29 +0000

    navar bootstrap styling import

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -16,8 +16,8 @@ function NavbarTest() {
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#pricing"></Nav.Link>
           </Nav>
         </Container>
       </Navbar>
