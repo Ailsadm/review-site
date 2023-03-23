f342529636f2a2a5317d73afb84264ddc452f42a
meri <mariana_asavoaiei@outlook.com>
2023-03-23 01:24:26 +0000

    fixed the review form and the button, still working on footer content

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./style.css";
-import logo from './logo.png'
+import logo from "./logo.png";
+
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Book Reviews</h1>
       <div className="logo-div">
-        <img classname="logo" src={logo} style={{ width: 80, height: 80 }} alt="logo with tagline" />
+        <img
+          classname="logo"
+          src={logo}
+          style={{ width: 88, height: 80 }}
+          alt="logo with tagline: Read freely"
+        />
       </div>
     </header>
   );
