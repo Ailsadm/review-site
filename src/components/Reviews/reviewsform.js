5c7e1f61b75d7d831d6ce994e9dca04b1fdd83ab
Kinori <russel.richard@outlook.com>
2023-03-24 10:24:37 +0000

    added validation for the reviews form

--- src/components/Reviews/reviewsform.js
+++ src/components/Reviews/reviewsform.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import "./style.css";
 
 function ReviewsForm({ submitHandler }) {
-   const [formData, setFormData] = useState({
-     name: "",
-     rating: "",
-     review: "",
-   });
+  const [formData, setFormData] = useState({
+    name: "",
+    rating: "",
+    review: "",
+  });
 
   const changeHandle = (fieldName, value) => {
     setFormData((previousState) => {
@@ -21,14 +21,14 @@ function ReviewsForm({ submitHandler }) {
     event.preventDefault();
     submitHandler(formData);
   };
-  
+
 
   return (
     <div className="formDiv">
       <form className="form" onSubmit={handleSubmit}>
         <h3>Add your review</h3>
         <input
-          
+          required
           className="inputform"
           value={formData.name}
           name="name"
@@ -44,45 +44,50 @@ function ReviewsForm({ submitHandler }) {
           checked={formData.rating === "1"}
           onChange={(e) => changeHandle("rating", "1")}
         />
-        <label htmlFor="regular">1</label>
-        <input
-          type="radio"
-          name="rating"
-          value="2"
-          id="2"
-          checked={formData.rating === "2"}
-          onChange={(e) => changeHandle("rating", "2")}
-        />
-        <label htmlFor="regular">2</label>
-        <input
-          type="radio"
-          name="rating"
-          value="3"
-          id="3"
-          checked={formData.rating === "3"}
-          onChange={(e) => changeHandle("rating", "3")}
-        />
-        <label htmlFor="regular">3</label>
-        <input
-          type="radio"
-          name="rating"
-          value="4"
-          id="4"
-          checked={formData.rating === "4"}
-          onChange={(e) => changeHandle("rating", "4")}
-        />
-        <label htmlFor="regular">4</label>
-        <input
-          type="radio"
-          name="rating"
-          value="5"
-          id="5"
-          checked={formData.rating === "5"}
-          onChange={(e) => changeHandle("rating", "5")}
-        /><label htmlFor="regular">5</label></div>
-        
-        
+          <label htmlFor="regular">1</label>
+          <input
+            required
+            type="radio"
+            name="rating"
+            value="2"
+            id="2"
+            checked={formData.rating === "2"}
+            onChange={(e) => changeHandle("rating", "2")}
+          />
+          <label htmlFor="regular">2</label>
+          <input
+            required
+            type="radio"
+            name="rating"
+            value="3"
+            id="3"
+            checked={formData.rating === "3"}
+            onChange={(e) => changeHandle("rating", "3")}
+          />
+          <label htmlFor="regular">3</label>
+          <input
+            required
+            type="radio"
+            name="rating"
+            value="4"
+            id="4"
+            checked={formData.rating === "4"}
+            onChange={(e) => changeHandle("rating", "4")}
+          />
+          <label htmlFor="regular">4</label>
+          <input
+            required
+            type="radio"
+            name="rating"
+            value="5"
+            id="5"
+            checked={formData.rating === "5"}
+            onChange={(e) => changeHandle("rating", "5")}
+          /><label htmlFor="regular">5</label></div>
+
+
         <textarea
+          required
           className="inputform"
           value={formData.review}
           name="review"
